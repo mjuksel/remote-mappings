c2267978f25fee8ddf393aac618f9e09b3cfef97
Mjuksel <mportma@gmail.com>
2023-01-22 04:33:26 +0100

    filter example

--- tsUsage.ts
+++ tsUsage.ts
@@ -1,14 +1,22 @@
 import { Devices } from './ReasonDevices.ts'
 import { sendMIDI, getRandomValue, objKeys } from './src/deps.ts'
 
-let currentDevice = Devices['se.propellerheads.Europa']
+const currentDevice = Devices['se.propellerheads.Europa']
 
-// let deviceParams = Object.keys(currentDevice['params'])
-let deviceParams = objKeys(currentDevice.params)
+const deviceParams = objKeys(currentDevice.params)
 
 /**
  * Loop over all parameters, sending random value to all of them.
  */
 deviceParams.forEach((value, index) => {
-	sendMIDI(index, getRandomValue(currentDevice.params[value]))
+	if (
+		value === 'Osc1 Wave' ||
+		value === 'Osc1 Shape' ||
+		value === 'Osc2 Wave' ||
+		value === 'Osc2 Shape' ||
+		value === 'Osc3 Wave' ||
+		value === 'Osc3 Shape'
+	) {
+		sendMIDI(index, getRandomValue(currentDevice.params[value]))
+	}
 })
