2fd9d23875bb25d1934caa5097f5b44af1edad49
Mjuksel <mportma@gmail.com>
2023-01-22 03:06:47 +0100

    remove unneeded stuff

--- src/deps.ts
+++ src/deps.ts
@@ -6,21 +6,9 @@ export { sendMIDI } from './sendMidi.ts'
 export const objKeys = <Obj>(object: Obj): (keyof Obj)[] =>
 	Object.keys(object) as (keyof Obj)[]
 
-/** Get random number between min and max */
-export const numBetween = (min: number, max: number) =>
-	Math.round(Math.random() * (max - min)) + min
-
-/** Get random value from steps, using min and max */
-export const getRandomValueMinMax = (min: number, max: number, steps: number) =>
-	~~((127 / steps) * numBetween(min, max))
-
 /** Get random value from steps. */
-export const getRandomValue = (steps: number) => {
-	// steps > 127 && (steps = 127)
-	return ~~(
-		(127 / steps > 127 ? 127 : steps) * Math.round(Math.random() * steps)
-	)
-}
+export const getRandomValue = (steps: number) =>
+	~~((127 / steps) * (Math.random() * steps))
 
 /** removes '\r' */
 export const format = (str: string) => str.replace(/\r?\n/g, '\n')
